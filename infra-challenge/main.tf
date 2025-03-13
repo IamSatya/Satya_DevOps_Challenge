d17dceaf1e5d94323fef1280a73560cabad84302
Satya <satyapanuganti@gmail.com>
2025-03-13 19:31:39 +0530

    added envs

--- infra-challenge/main.tf
+++ infra-challenge/main.tf
@@ -9,7 +9,7 @@ terraform {
     resource_group_name  = "tfbackend-fmc"  # Can be passed via `-backend-config=`"resource_group_name=<resource group name>"` in the `init` command.
     storage_account_name = "tfbackendfmcsatya"                      # Can be passed via `-backend-config=`"storage_account_name=<storage account name>"` in the `init` command.
     container_name       = "tfstate"                       # Can be passed via `-backend-config=`"container_name=<container name>"` in the `init` command.
-    key                  = "${var.region}/${var.ENV}/app.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
+    key                  = "var.region/var.ENV/app.terraform.tfstate"        # Can be passed via `-backend-config=`"key=<blob key name>"` in the `init` command.
   }
 }
 
