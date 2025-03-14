3213665e0bbba9d0e129ee70f6cce345bee2b3b3
Satya <satyapanuganti@gmail.com>
2025-03-14 13:13:32 +0530

    added source abd dest ips

--- infra-challenge/vnet.tf
+++ infra-challenge/vnet.tf
@@ -60,8 +60,8 @@ resource "azurerm_network_security_group" "myNSG" {
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
-    source_address_prefix      = "*"
-    destination_address_prefix = "*"
+    source_address_prefix      = "10.0.0.0/16"
+    destination_address_prefix = "10.0.0.0/16"
   }
 }
 
